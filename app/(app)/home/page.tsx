8704e060b5d3c0ed41eb85e2659b5ad5afb0a88e
Manyamsanjaykumarreddy <entrepreneur.sanju@gmail.com>
2024-08-18 23:01:51 +0530

    public id fix

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -53,7 +53,7 @@ function Home() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {videos.map((video) => (
             <VideoCard
-              key={video.id}
+              key={video.publicId}
               video={video}
               onDownload={handleDownload}
             />
